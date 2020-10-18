fefa48e3642a256cafe8bb0292af76adb06f4d37
Janick <janick.huerzeler@students.fhnw.ch>
2020-10-18 12:58:56 +0200

    Map user.id to userId

--- src/main/java/ch/fhnw/eaf/mappers/MovieMapper.java
+++ src/main/java/ch/fhnw/eaf/mappers/MovieMapper.java
@@ -8,14 +8,15 @@ import ch.fhnw.eaf.rental.dto.UserDto;
 import ch.fhnw.eaf.rental.model.Rental;
 import ch.fhnw.eaf.rental.model.User;
 
-@Mapper(componentModel="spring")
+@Mapper(componentModel = "spring")
 public interface MovieMapper {
 
 	@Mapping(source = "rentals", target = "rentalIds")
 	UserDto userToUserDto(User user);
-	
+
+	@Mapping(source = "user.id", target = "userId")
 	RentalDto rentalToRentalDto(Rental rental);
-	
+
 	default Long rentalToLong(Rental r) {
 		return r.getId();
 	}
